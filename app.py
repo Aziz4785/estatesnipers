bc10c36edd47d084dd8d79d8cda3754c9bfadf45
Azizk <azizkanoun3@gmail.com>
2024-04-24 01:03:13 +0200

    modify file path

--- app.py
+++ app.py
@@ -278,11 +278,8 @@ def dubai_areas():
         ]
         }
         
-        base_dir = os.path.dirname(os.path.abspath(__file__))
-        file_path = os.path.join(base_dir, 'areas_coordinates', 'DubaiAreas.geojson')
-        
         # Load the GeoJSON file
-        with open(file_path, 'r') as file:
+        with open('areas_coordinates/dubaiAreas.geojson', 'r') as file:
             geojson = json.load(file)
     
         # Enrich GeoJSON with price data and calculate fill colors
